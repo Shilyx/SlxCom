d158573f9a246f613db01a4c182f332d2c3fd70b
Administrator <Administrator@95e5b859-71cd-234a-b975-0ecc2f9501a4>
2012-02-10 09:46:14 +0000

    增加Default.jpg

--- SlxCom/resource.h
+++ SlxCom/resource.h
@@ -2,20 +2,18 @@
 // Microsoft Visual C++ generated include file.
 // Used by SlxCom.rc
 //
-#define IDR_DEFAULT1                    101
-#define IDI_ICON1                       103
-#define IDR_RT_FILE1                    104
 #define IDR_ICON                        104
 #define IDR_FILE                        105
 #define IDB_INSTALL                     106
 #define IDB_UNINSTALL                   107
 #define IDB_COMBINE                     108
+#define IDR_DEFAULTJPG                  109
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_RESOURCE_VALUE        110
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
