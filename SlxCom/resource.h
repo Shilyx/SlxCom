c82acec88233bfd6eee18192c6a149cc7b5cbaa9
Administrator <Administrator@95e5b859-71cd-234a-b975-0ecc2f9501a4>
2012-02-16 03:11:25 +0000

    新增三个位图

--- SlxCom/resource.h
+++ SlxCom/resource.h
@@ -8,12 +8,15 @@
 #define IDB_UNINSTALL                   107
 #define IDB_COMBINE                     108
 #define IDR_DEFAULTJPG                  109
+#define IDB_COPYFULLPATH                110
+#define IDB_ADDTOCOPY                   111
+#define IDB_ADDTOCUT                    112
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        110
+#define _APS_NEXT_RESOURCE_VALUE        113
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
