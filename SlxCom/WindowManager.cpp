7d74efd769462cb7f8cf1b2ee90eb42f10c005a3
local <local@95e5b859-71cd-234a-b975-0ecc2f9501a4>
2014-06-29 09:37:53 +0000

    新增创建管理器窗口

--- SlxCom/WindowManager.cpp
+++ SlxCom/WindowManager.cpp
@@ -1,14 +1,53 @@
 #include "WindowManager.h"
 #include "SlxComTools.h"
 
+#define NOTIFYWNDCLASS TEXT("__slx_WindowManager_20140629")
+
+class CNotifyClass
+{
+public:
+    CNotifyClass(HWND hManagerWindow, HWND hTargetWindow);
+    ~CNotifyClass();
+
+public:
+
+private:
+
+};
+
 class CWindowManager
 {
 public:
-    CWindowManager()
+    CWindowManager(HINSTANCE hInstance)
     {
-        RegisterWindowClass();
+        m_hInstance = hInstance;
+        RegisterWindowClass(m_hInstance);
+        m_hWindow = CreateWindowEx(
+            0,
+            NOTIFYWNDCLASS,
+            NOTIFYWNDCLASS,
+            WS_OVERLAPPEDWINDOW,
+            0, 0, 377, 55,
+            NULL,
+            NULL,
+            m_hInstance,
+            this
+            );
+
+        if (m_hWindow)
+        {
+            ShowWindow(m_hWindow, SW_SHOW);
+            UpdateWindow(m_hWindow);
+        }
     }
-    ~CWindowManager();
+
+    ~CWindowManager()
+    {
+        if (IsWindow(m_hWindow))
+        {
+            DestroyWindow(m_hWindow);
+        }
+    }
 
     void Run()
     {
@@ -29,23 +68,55 @@ public:
     }
 
 private:
-    static void RegisterWindowClass()
+    static void RegisterWindowClass(HINSTANCE hInstance)
     {
+        WNDCLASSEX wcex = {sizeof(wcex)};
+        wcex.lpfnWndProc = WindowManagerWindowProc;
+        wcex.lpszClassName = NOTIFYWNDCLASS;
+        wcex.hInstance = hInstance;
+        wcex.style = CS_HREDRAW | CS_VREDRAW;
 
+        RegisterClassEx(&wcex);
+    }
+
+    static CWindowManager *GetClass(HWND hWnd)
+    {
+        return (CWindowManager *)GetWindowLongPtr(hWnd, GWLP_USERDATA);
     }
 
     static LRESULT WINAPI WindowManagerWindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
     {
+        switch (uMsg)
+        {
+        case WM_CREATE:
+            SetWindowLongPtr(hWnd, GWLP_USERDATA, (LONG_PTR)((LPCREATESTRUCT)lParam)->lpCreateParams);
+            return 0;
+
+        case WM_CLOSE:
+            DestroyWindow(hWnd);
+            break;
+
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+
+        default:
+            break;
+        }
+
         return DefWindowProc(hWnd, uMsg, wParam, lParam);
     }
 
 private:
     HWND m_hWindow;
+    HINSTANCE m_hInstance;
 };
 
 static DWORD CALLBACK WindowManagerProc(LPVOID lpParam)
 {
-    CWindowManager().Run();
+    HINSTANCE hInstance = (HINSTANCE)lpParam;
+
+    CWindowManager(hInstance).Run();
     return 0;
 }
 
